ad66abc8f56251a0fdff9f05807562ec33b99054
Ivan Gonzalez <iv.ed.gonzalez@gmail.com>
2017-11-12 00:09:04 -0500

    finally finished working spotify player display!

--- awesome/spotify.lua
+++ awesome/spotify.lua
@@ -19,10 +19,10 @@ local function construct(args)
     local settings = args.settings or function() end
     
     metadata = {
-        status = "N/A",
-        title  = "N/A",
-        artist = "N/A",
-        album  = "N/A",
+        status = nil,
+        title  = nil,
+        artist = nil,
+        album  = nil,
     }
     
     function spotify.update()
@@ -31,11 +31,6 @@ local function construct(args)
         local get_artist = "playerctl metadata xesam:artist"
         local get_album  = "playerctl metadata xesam:album"
         
-        cmds = {
-            { metadata.title, get_title },
-            { metadata.artist, get_artist },
-            { metadata.album, get_album },
-        }
 
         awful.spawn.easy_async(get_status, 
             function(stdout,_,_,_,_)
@@ -43,12 +38,20 @@ local function construct(args)
             end)
 
         if metadata.status then
-            for _,v in pairs(cmds) do
-                awful.spawn.easy_async(v[2],
-                    function(stdout,_,_,_)
-                        v[1] = stdout
-                    end)
-            end
+            awful.spawn.easy_async(get_title, 
+                function(stdout,_,_,_,_)
+                    metadata.title = stdout
+                end)
+
+            awful.spawn.easy_async(get_artist, 
+            function(stdout,_,_,_,_)
+                metadata.artist = stdout
+            end)
+
+            awful.spawn.easy_async(get_album, 
+            function(stdout,_,_,_,_)
+                metadata.album = stdout
+            end)
         else return end
 
         widget = spotify.widget
@@ -61,57 +64,3 @@ local function construct(args)
 end
 
 return construct
-
---[[    set_image = function(self, path)
-        self.icon.image = path
-    end,[
-    set_markup = function(self, path)
-        self.current_song.markup = path
-    end,
-}
-
-
-
-
-
-
-
-
-local update_widget_icon = function(widget, stdout, _, _, _)
-    stdout = string.gsub(stdout, "\n", "")
-    if (stdout == 'RUNNING') then
-        widget:set_image(PATH_TO_ICONS .. "/play.png")--"/24/actions/player_play.svg")
-    elseif (stdout == "CORKED") then
-        widget:set_image(PATH_TO_ICONS .. "/pause.png")--"/24/actions/player_pause.svg")
-    else
-        widget:set_image(nil)
-    end
-end
-
-local update_widget_text = function(widget, stdout, _, _, _)
-    if string.find(stdout, 'Error: Spotify is not running.') ~= nil then
-        widget:set_markup('')
-        widget:set_visible(false)
-    else
-        widget:set_markup("<span fgcolor='#474747' weight='bold'>" .. stdout .. "</span>")
-        widget:set_visible(true)
-    end
-end
-
-watch(GET_SPOTIFY_STATUS_CMD, 1, update_widget_icon, spotify_widget)
-watch(GET_CURRENT_SONG_CMD, 1, update_widget_text, spotify_widget)
---]]
---[[
--- Adds mouse control to the widget:
---  - left click - play/pause
---  - scroll up - play next song
---  - scroll down - play previous song --]]
---[[spotify_widget:connect_signal("button::press", function(_, _, _, button)
-    if (button == 1) then awful.spawn("sp play", false)      -- left click
-    elseif (button == 4) then awful.spawn("sp next", false)  -- scroll up
-    elseif (button == 5) then awful.spawn("sp prev", false)  -- scroll down
-    end
-    awful.spawn.easy_async(GET_SPOTIFY_STATUS_CMD, function(stdout, stderr, exitreason, exitcode)
-        update_widget_icon(spotify_widget, stdout, stderr, exitreason, exitcode)
-    end)
-end)--]]
